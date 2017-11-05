e1adea3c964912bc92dcd32737c68600b5c01f10
Yuchen Lu <luyuchen@elisa1.iro.umontreal.ca>
2017-11-04 21:25:39 -0400

    start sweeping args

--- part2/main.py
+++ part2/main.py
@@ -7,14 +7,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+import argparse
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
 
 #########CONFIG####################
+parser = argparse.ArgumentParser()
+parser.add_argument("--bsz", default=200, type=int)
+parser.add_argument("--mode", default=0, type=int)
+parser.add_argument("--num_epochs", default=20, type=int)
+parser.add_argument("--lr", default=0.01, type=float)
+parser.add_argument("--out", default="./result/", type=str)
+parser.add_argument("--log_interval", default=5, type=int)
+args = parser.parse_args()
 use_cuda = torch.cuda.is_available()
-batch_size = 100
-mode = 0 # preprocessing mode
-num_epochs = 20
-lr = 0.01
-log_interval = 5
 ##################################
 
 ################DEFINITION#########
@@ -45,6 +54,7 @@ class MLP(nn.Module):
         logprobs = F.log_softmax(out)
         return logprobs
 
+
 def _to_torch(data, label):
     if use_cuda:
         data = Variable(torch.Tensor(data)).cuda()
@@ -53,6 +63,7 @@ def _to_torch(data, label):
         data = Variable(torch.Tensor(data))
         label = Variable(torch.LongTensor(label))
     return data, label
+
 
 def eval_epoch(data_iter, model):
     model.eval()
@@ -67,6 +78,7 @@ def eval_epoch(data_iter, model):
         num_correct += (pred==label).sum().data[0]
 
     return num_correct*100 / float(num_data)
+
 
 def train_epoch(data_iter, model, optim):
     model.train()
@@ -85,7 +97,7 @@ def train_epoch(data_iter, model, optim):
         pred = torch.max(logprobs, -1)[1]
         num_correct += (pred==label).sum().data[0]
 
-        if (i+1) % log_interval == 0:
+        if (i+1) % args.log_interval == 0:
             print("batch {} nll_loss {:.2f}, acc {:.2f}%".format(
                 i+1, nll.data[0], num_correct*100/float(num_data)
                 ))
@@ -97,21 +109,34 @@ def train_epoch(data_iter, model, optim):
 
 ###############DATA, MODEL, OPTIM########
 start = time.time()
-train_iter, test_iter = data_input(batch_size, mode)
+train_iter, test_iter = data_input(args.bsz, args.mode)
 print("load data cost {0:.2f} sec".format(time.time()-start))
 
 model = MLP(preprocessing.vocab_size, 20)
 if use_cuda:
     model.cuda()
 
-optim = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+optim = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
 
 ######################################
-
-for epoch in range(num_epochs):
+train_accs = []
+test_accs = []
+for epoch in range(args.num_epochs):
     print('-----------Epoch {}---------'.format(epoch))
     train_acc = train_epoch(train_iter, model, optim)
-    eval_acc = eval_epoch(test_iter, model)
+    test_acc = eval_epoch(test_iter, model)
     print("epoch {} train_acc {:.2f}% eval_acc {:.2f}%".format(
-        epoch+1, train_acc, eval_acc
+        epoch+1, train_acc, test_acc
         ))
+    train_accs.append(train_acc)
+    test_accs.append(test_acc)
+
+if not os.path.isdir(args.out):
+    os.mkdir(args.out)
+
+plt.figure()
+epochs = [ e+1 for e in range(args.num_epochs) ]
+train_plt, = plt.plot(epochs, train_accs, "r-")
+test_plt, = plt.plot(epochs, test_accs, "b-")
+plt.legend([train_plt, test_plt], ["train acc", "test acc"])
+plt.savefig(os.path.join(args.out, "acc.png"))
---
+++ part2/var_in_train.py
@@ -0,0 +1,96 @@
+import preprocessing
+from preprocessing import data_input
+
+import ipdb
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import argparse
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+
+#########CONFIG####################
+mode = 0
+lr = 0.2
+bsz = 100
+use_cuda = torch.cuda.is_available()
+##################################
+
+################DEFINITION#########
+class MLP(nn.Module):
+    def __init__(self, num_inputs, num_classes):
+        super().__init__()
+        self.linear1 = nn.Linear(num_inputs, 100)
+        self.linear1.bias.data.zero_()
+        nn.init.xavier_uniform(
+                self.linear1.weight.data,
+                gain=nn.init.calculate_gain('relu')
+                )
+
+        self.linear2 = nn.Linear(100, num_classes)
+        self.linear2.bias.data.zero_()
+        nn.init.xavier_uniform(
+                self.linear2.weight.data,
+                )
+
+
+    def forward(self, inputs):
+        """
+        inputs:[bsz, num_inputs]
+        return: logprobs [bsz, num_classes]
+        """
+        out = F.relu(self.linear1(inputs))
+        out = self.linear2(out)
+        logprobs = F.log_softmax(out)
+        return logprobs
+
+
+def _to_torch(data, label):
+    if use_cuda:
+        data = Variable(torch.Tensor(data)).cuda()
+        label = Variable(torch.LongTensor(label)).cuda()
+    else:
+        data = Variable(torch.Tensor(data))
+        label = Variable(torch.LongTensor(label))
+    return data, label
+
+########################################
+
+
+
+###############batch_size 1 ########
+start = time.time()
+train_iter, _ = data_input(bsz, mode)
+print("load data cost {0:.2f} sec".format(time.time()-start))
+
+model = MLP(preprocessing.vocab_size, 20)
+if use_cuda:
+    model.cuda()
+
+optim = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+######################################
+losses = []
+for step in range(5000):
+    try:
+        data, label = train_iter.__next__()
+    except StopIteration:
+        train_iter.__iter__()
+        data, label = train_iter.__next__()
+
+    data, label = _to_torch(data, label)
+    logprobs = model(data)
+
+    nll = F.nll_loss(logprobs, label)
+    optim.zero_grad()
+    nll.backward()
+    optim.step()
+
+    losses.append(nll.data[0])
+
+pickle.dump(losses, open("train_loss{}.pkl".format(bsz), "wb"))
