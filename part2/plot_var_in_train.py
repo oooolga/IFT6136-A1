109e92f8a11d9935429bacb80f5f5f9cbf1532b3
Yuchen Lu <luyuchen@elisa1.iro.umontreal.ca>
2017-11-06 16:00:37 -0500

    change plot

--- part2/plot_var_in_train.py
+++ part2/plot_var_in_train.py
@@ -2,13 +2,14 @@ import pickle
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 
-loss_100 = pickle.load(open("train_loss100.pkl", "rb"))
-loss_1 = pickle.load(open("train_loss1.pkl", "rb"))
+loss_100 = np.array(pickle.load(open("train_loss100.pkl", "rb")))
+loss_1 = np.array(pickle.load(open("train_loss1.pkl", "rb")))
 plt.figure()
-steps = [ i+1 for i in range(5000)]
-plt_100, = plt.plot(steps, loss_100, 'rx')
-plt_1, = plt.plot(steps, loss_1, 'bx')
+steps = np.arange(0,5000,20)
+plt_100, = plt.plot(steps, loss_100[steps], 'rx')
+plt_1, = plt.plot(steps, loss_1[steps], 'bx')
 plt.legend([plt_100, plt_1], ["batch size 100","batch size 1"])
 plt.xlabel("Steps")
 plt.ylabel("Loss")
