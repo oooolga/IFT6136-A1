65f5bee65b0ca62faa6373ffb5a3fbfeff046de2
Yuchen Lu <luyuchen.paul@gmail.com>
2018-02-10 20:56:36 -0500

    add tfidf

--- part2/preprocessing.py
+++ part2/preprocessing.py
@@ -1,6 +1,12 @@
 import ipdb
 import numpy as np
 import pickle
+import argparse
+import math
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", type=int, default=0, help="0: not processing. 1: tf_idf. 2: standardization")
+args = parser.parse_args()
 
 idx2word = open("./raw/vocabulary.txt", "r").read().splitlines()
 vocab_size = len(idx2word)
@@ -29,15 +35,28 @@ def read_data(data_file, label_file):
     label = np.array([int(line) for line in label_lines ])
     return data, label
 
-def tfidf(data):
-    """
-    Term frequency use normalized term freq.
-    IDF use logarithmically scaled inverse fraction of the documents that contain the word.
-    """
-    ipdb.set_trace()
-    #tf = np.log(data)
-
 train_data, train_label = read_data("./raw/train.data", "./raw/train.label")
 test_data, test_label = read_data("./raw/test.data", "./raw/test.label")
+num_doc = len(train_label)
 
-train_tfidf = tfidf(train_data)
+if args.mode == 1:
+    # compute IDF for each term
+    for word in range(vocab_size):
+        num_doc_has_term = 0
+        for doc in range(num_doc):
+            if train_data[doc, word] > 0:
+                num_doc_has_term += 1
+        idf = math.log(num_doc / float(num_doc_has_term))
+        ipdb.set_trace()
+
+        # modify train_data, test_data
+        train_data[:, word] = train_data[:, word] * idf
+        test_data[:, word] = test_data[:, word] * idf
+    raise NotImplementedError
+if args.mode == 2:
+    raise NotImplementedError
+else:
+    pickle.dump({"train":(train_data, train_label), "test":(test_data, test_label)}, open("data.dat", "wb"))
+
+
+
