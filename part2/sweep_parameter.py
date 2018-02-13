a267014263f067ee961c26ba068f785ba88c2ea7
Yuchen Lu <luyuchen.paul@gmail.com>
2018-02-12 20:29:13 -0500

    add lr

--- part2/sweep_parameter.py
+++ part2/sweep_parameter.py
@@ -1,1 +1,11 @@
+from subprocess import call
+import os
+
 root_dir = "./exps"
+lrs = [0.001, 0.01, 0.05]
+
+for lr in lrs:
+    out_dir = os.path.join(root_dir, "lr{}".format(lr))
+    cmd = ["python", "main.py", "--lr", str(lr), "--out", out_dir]
+    call(cmd)
+
