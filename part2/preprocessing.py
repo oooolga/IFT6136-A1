a0b11f50167f9f8da179aea1ad7e04874136f9b0
Yuchen Lu <luyuchen.paul@gmail.com>
2018-02-12 23:06:53 -0500

    use sparsity to preprocess

--- part2/preprocessing.py
+++ part2/preprocessing.py
@@ -25,13 +25,35 @@ def read_data(data_file, label_file):
     data_lines = open(data_file, "r").read().splitlines()
     label_lines = open(label_file, "r").read().splitlines()
     num_docs = _doc_id(data_lines[-1]) + 1
-    assert num_docs == len(label_lines)
-
     data = torch.zeros(num_docs, vocab_size)
+
     for line in data_lines:
         data[_doc_id(line), _word_id(line)] += _freq(line)
+
     label = np.array([int(line)-1 for line in label_lines ])
     return data, torch.LongTensor(label)
+
+def _get_stats(data_file):
+    """
+    :return: idf [vocab_size], mean [vocab_size] sigma [vocab_size]
+    """
+    data_lines = open(data_file, "r").read().splitlines()
+    num_doc_contains_term = torch.zeros(vocab_size)
+    num_docs = _doc_id(data_lines[-1]) + 1
+    sum = torch.zeros(vocab_size)
+    sq_sum = torch.zeros(vocab_size)
+
+    for line in data_lines:
+        num_doc_contains_term[_word_id(line)] += 1
+        sum[_word_id(line)] += _freq(line)
+        sq_sum[_word_id(line)] += _freq(line)**2
+
+    mean = sum / num_docs
+    sq_mean = sq_sum / num_docs
+    sigma = torch.sqrt(sq_mean - mean.pow(2))
+
+    idf = - torch.log(num_doc_contains_term / num_docs)
+    return idf, mean, sigma
 
 class data_iter:
     def __init__(self, data, label, bsz):
@@ -68,37 +90,15 @@ def data_input(batch_size, mode=0):
     print("reading data...")
     train_data, train_label = read_data("./raw/train.data", "./raw/train.label")
     test_data, test_label = read_data("./raw/test.data", "./raw/test.label")
+    idf, mean, sigma = _get_stats("./raw/train.data")
     if mode == 1:
-        #ipdb.set_trace()
-        if os.path.isfile("./idf.pkl"):
-            print("loading idf...")
-            idf = pickle.load(open("./idf.pkl", "rb"))
-        else:
-            print("computing idf...")
-            ipdb.set_trace()
-            corpus = torch.cat([train_data, test_data])
-            idf = torch.sum((corpus > 0), 0)
-            idf = -torch.log(idf / len(corpus))
-            pickle.dump(idf, open("./idf.pkl", "wb"), protocol=4)
-        print("computing tfidf...")
         train_data = train_data * idf
         test_data = test_data * idf
 
     if mode == 2:
         eps = 1e-5
-        if os.path.isfile("./stats.pkl"):
-            mean, sigma = pickle.load(open("./stats.pkl", 'rb'))
-        else:
-            mean = torch.mean(train_data, 0)
-            sigma = torch.sqrt(torch.var(train_data, 0))
-            pickle.dump(
-                    (mean, sigma),
-                    open('./stats.pkl', 'wb'),
-                    protocol=4,
-                    )
         train_data = (train_data - mean) / (sigma + eps)
         test_data = (test_data - mean) / (sigma + eps)
-
 
     train_iter = data_iter(train_data, train_label, batch_size)
     test_iter = data_iter(test_data, test_label, batch_size)
